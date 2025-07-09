9ac0a412945c3aa9757242a823819c8a24eae38f
Laís Dutra <dutra.laisgalvao@gmail.com>
2025-07-09 17:27:30 -0300

    Update BubbleSort

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,30 +1,32 @@
 /*
-- Percorre o vetor várias vezes;
+- Percorre o vetor repetidas vezes;
 
-- A cada passagem faz flutuar para o topo da lista o maior elemento;
+- A cada passagem, faz o maior elemento “flutuar” para o final do vetor;
 
-- Compara pares de elementos adjacentes e os troca de lugar se estiverem na ordem errada;
+- Compara pares de elementos adjacentes e os troca de lugar se estiverem fora de ordem;
 
-- O processo se repete até que mais nenhuma troca seja necessária;
+- O processo se repete até que não sejam mais necessárias trocas;
 
-- Melhor caso: O(n)
-- Caso médio: O(n^2)
-- Pior caso: O(n^2)
-- Não recomendado para grandes conjuntos de dados
-- Não recomendado para programas que precisem de velocidade
+- Complexidade:
+  Melhor caso:   O(n)
+  Caso médio:    O(n^2)
+  Pior caso:     O(n^2)
+
+- Não recomendado para conjuntos de dados grandes;
+- Não recomendado para aplicações que exigem alta performance.
 */
 
-void bubbleSort(int* v, int n){
+void bubbleSort(int* vetor, int tamanho){
     bool houveTroca = true;
-    int aux;
+    int temporario;
 
-    while(houveTroca == true){
+    while (houveTroca == true){
         houveTroca = false;
-        for(int i=0; i < n-1; i++){
-            if(v[i] > v[i+1]){
+        for (int i = 0; i < n-1; i++){
+            if (v[i] > v[i+1]){
                 aux = v[i];
                 v[i] = v[i+1];
-                v[i+1] = aux;
+                v[i+1] = temporario;
                 houveTroca = true;
             }
         }
