5950ce40f18a3715624bd7a8c89745b487839abb
Laís Dutra <dutra.laisgalvao@gmail.com>
2025-07-09 01:04:59 -0300

    Create sort.h

---
+++ sort.h
@@ -0,0 +1,13 @@
+#ifndef SORT_H_INCLUDED
+#define SORT_H_INCLUDED
+
+void bubbleSort(int* v, int n);
+void combSort(int* v, int n);
+void countingSort(int* v, int n, int k);
+void heapSort(int* v, int n);
+void insertionSort(int* v, int n);
+void mergeSort(int* v, int c, int f);
+void quickSort(int* v, int c, int f);
+void selectionSort(int* v, int n);
+
+#endif
